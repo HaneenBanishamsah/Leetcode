ff69d80e62a41e17e4457500ccf6a53f916e7a33
HaneenBanishamsah <94951337+HaneenBanishamsah@users.noreply.github.com>
2024-08-20 09:50:10 +0300

    Time: 11 ms (53.80%) | Memory: 13 MB (47.00%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+       
+        unordered_map<char, int> roman_to_int = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int total = 0;
+        int prev_value = 0;
+
+        
+        for (int i = 0; i < s.length(); ++i) {
+            int value = roman_to_int[s[i]];
+
+         
+            if (value > prev_value) {
+                total += value - 2 * prev_value;
+            } else {
+                total += value;
+            }
+
+           
+            prev_value = value;
+        }
+
+        return total;
+    }
+};
